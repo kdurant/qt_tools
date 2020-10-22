1e370aa94b3f57d9f1371d989b5f70ef02efdf9a
kdurant <wangjun850725@163.com>
2020-10-22 22:37:40 +0800

    feat: change receive data between HEX and ASCII

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,6 +10,7 @@ MainWindow::MainWindow(QWidget *parent)
 
     initParameter();
     serialPortDetect();
+    connectSignalSolt();
 }
 
 MainWindow::~MainWindow()
@@ -20,6 +21,7 @@ MainWindow::~MainWindow()
 // configIni->value("System/oceanPort")
 void MainWindow::initParameter()
 {
+    uartRecv.textFormat = ui->rbt_recvHexFormat->isChecked() ? UartRecv::HEX : UartRecv::ASCII;
 }
 
 //configIni->setValue("Laser/freq", 1111);
@@ -29,6 +31,12 @@ void MainWindow::saveParameter()
     configIni->setValue("Laser/freq", 1111);
 }
 
+void MainWindow::connectSignalSolt()
+{
+    connect(ui->rbt_recvHexFormat, SIGNAL(clicked(bool)), this, SLOT(setRecvTextFormat()));
+    connect(ui->rbt_recvAsciiFormat, SIGNAL(clicked(bool)), this, SLOT(setRecvTextFormat()));
+}
+
 void MainWindow::serialPortDetect()
 {
     QList<QSerialPortInfo> port = QSerialPortInfo::availablePorts();
@@ -36,6 +44,21 @@ void MainWindow::serialPortDetect()
     for(int i = 0; i < port.length(); i++)
         ui->serialPortComb->addItem(port.at(i).portName());
     return;
+}
+
+void MainWindow::setRecvTextFormat()
+{
+    uartRecv.textFormat = ui->rbt_recvHexFormat->isChecked() ? UartRecv::HEX : UartRecv::ASCII;
+
+    QString recvData = ui->pte_recvData->toPlainText();
+    if(uartRecv.textFormat == UartRecv::HEX)
+    {
+        ui->pte_recvData->setPlainText(recvData.toUtf8().toHex());
+    }
+    else
+    {
+        ui->pte_recvData->setPlainText(QByteArray::fromHex(recvData.toUtf8()));
+    }
 }
 
 void MainWindow::closeEvent(QCloseEvent *event)
--- mainwindow.h
+++ mainwindow.h
@@ -6,6 +6,28 @@
 #include <QSerialPort>
 #include <QSerialPortInfo>
 
+class UartSend
+{
+public:
+    int  textFormat;
+    bool autoSend;
+    int  autoSendTime;  // unit us
+    bool addNewLine;
+};
+
+class UartRecv
+{
+public:
+    enum TextFormat
+    {
+        HEX = 0,
+        ASCII
+    };
+    TextFormat textFormat;
+    bool       addNewLine;
+    bool       addTimestamp;
+};
+
 QT_BEGIN_NAMESPACE
 namespace Ui
 {
@@ -25,12 +47,18 @@ public:
 
     void initParameter();
     void saveParameter();
+    void connectSignalSolt();
 
     // 串口相关函数
     void serialPortDetect();
 
+public slots:
+    void setRecvTextFormat();
+
 private:
     Ui::MainWindow *ui;
     QSettings *     configIni;
+
+    UartRecv uartRecv;
 };
 #endif  // MAINWINDOW_H
